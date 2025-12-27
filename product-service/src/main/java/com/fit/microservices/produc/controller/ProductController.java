b117d42eda258a8de6129c25f8a825f7415a2405
buikhanhlan1410 <buikhanhlan1410@gmail.com>
2025-12-28 00:54:25 +0700

    feat(swagger): add swagger open API for service

--- product-service/src/main/java/com/fit/microservices/produc/controller/ProductController.java
+++ product-service/src/main/java/com/fit/microservices/produc/controller/ProductController.java
@@ -3,6 +3,8 @@ package com.fit.microservices.produc.controller;
 import com.fit.microservices.produc.dto.ProductRequest;
 import com.fit.microservices.produc.dto.ProductResponse;
 import com.fit.microservices.produc.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cache.annotation.Cacheable;
@@ -10,30 +12,34 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@Tag(name = "Product API", description = "Operations related to products")
 @RestController
 @RequestMapping("/api/product")
 @RequiredArgsConstructor
 public class ProductController {
     private  final ProductService productService;
 
+    @Operation(summary = "Create new product")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public ProductResponse createProduct(@Valid  @RequestBody ProductRequest productRequest) {
         return productService.save(productRequest);
     }
 
+    @Operation(summary = "Get all product")
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<ProductResponse> findAllProducts() {
         return productService.findAll();
     }
 
+    @Operation(summary = "Update product")
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public ProductResponse updateProduct(@PathVariable Long id,@Valid @RequestBody ProductRequest productRequest) {
         return productService.update(id, productRequest);
     }
+    @Operation(summary = "Delete product by ID")
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public void deleteProduct(@PathVariable Long id) {
--- user-service/src/main/java/com/fit/microservices/user/controller/UserController.java
+++ user-service/src/main/java/com/fit/microservices/user/controller/UserController.java
@@ -5,6 +5,8 @@ import com.fit.microservices.user.dto.UserResponse;
 import com.fit.microservices.user.model.User;
 import com.fit.microservices.user.repository.UserRepository;
 import com.fit.microservices.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -12,13 +14,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@Tag(name = "User API", description = "Operations related to users")
 @RestController
 @RequestMapping("/api/user")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
     private final UserRepository userRepository;
+    @Operation(summary = "Get user by ID")
     @GetMapping("/{id}")
     public ResponseEntity<UserResponse> getUserById(@PathVariable Long id){
         return new ResponseEntity<>(userService.getUserById(id),HttpStatus.OK);
@@ -29,6 +32,7 @@ public class UserController {
                 .orElseThrow(() -> new RuntimeException("User not found"));
     }
     @PostMapping
+    @Operation(summary = "Create new user")
     public UserResponse createUser(@Valid  @RequestBody UserRequest request) {
         User user = User.builder()
                 .fullName(request.getFullName())
